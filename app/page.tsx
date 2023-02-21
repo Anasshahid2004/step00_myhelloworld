cddb18dcdae30696b11bf6d683f4d9d77429822b
Anasshahid2004 <anas.shahid.990@gmail.com>
2023-02-21 11:58:31 +0500

    Added my all files
    commit files

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google"
+// import  Style  from "app/page.module.css";   
+
+
+export default function Home() {
+           return (
+            <h1>Hello world</h1>
+           )
+}
